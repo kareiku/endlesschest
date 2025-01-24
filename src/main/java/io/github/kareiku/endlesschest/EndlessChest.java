003f33d6cbbd90622ac01c9c11e3b790c55e4723
kareiku <xaxvu7@gmail.com>
2025-01-24 23:50:55 +0100

    .

---
+++ src/main/java/io/github/kareiku/endlesschest/EndlessChest.java
@@ -0,0 +1,36 @@
+package io.github.kareiku.endlesschest;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+@Mod(EndlessChest.MODID)
+public class EndlessChest {
+    public static final String MODID = "endlesschest";
+
+    private static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(EndlessChest.MODID);
+    private static final DeferredBlock<Block> ENDLESS_CHEST = BLOCKS.register("Endless Chest", () ->
+            new Block(BlockBehaviour.Properties.of()));
+
+    public EndlessChest(IEventBus modEventBus, ModContainer modContainer) {
+    }
+}
